be38b961093ea48adf50e395b345e598230c42f3
liuwu513 <liuwu513@163.com>
2018-04-16 01:11:01 +0800

    前后台功能联动扩展

---
+++ pls-manage/src/page/order/addSubOrder.vue
@@ -0,0 +1,378 @@
+<template>
+    <div>
+     	<head-top></head-top>
+        <el-row style="margin-top: 20px;">
+  			<el-col :span="14" :offset="4">
+  				<header class="form_header">选择食品分类</header>
+	  			<el-form :model="categoryForm" :rules="categoryRules" ref="categoryForm" label-width="110px" class="form">
+		  			<el-row class="species_select">
+		  				<el-form-item label="食品分类">
+			  				<el-select v-model="categoryForm.categorySelect" :placeholder="selectValue.label" style="width:100%;">
+							    <el-option
+							      	v-for="item in categoryForm.categoryList"
+							      	:key="item.index"
+							      	:label="item.label"
+							      	:value="item.value">
+							    </el-option>
+						  	</el-select>
+						</el-form-item>
+					</el-row>
+					<el-row class="add_category_row" :class="showAddCategory? 'showEdit': ''">
+						<div class="add_category">
+							<el-form-item label="食品分类" prop="name">
+								<el-input v-model="categoryForm.name"></el-input>
+							</el-form-item>
+							<el-form-item label="分类描述" prop="remarks">
+								<el-input v-model="categoryForm.remarks"></el-input>
+							</el-form-item>
+							<el-form-item>
+								<el-button type="primary" @click="submitCategoryForm('categoryForm')">提交</el-button>
+							</el-form-item>
+						</div>
+					</el-row>
+					<div class="add_category_button" @click="addCategoryFun">
+						<i class="el-icon-caret-top edit_icon" v-if="showAddCategory"></i>
+						<i class="el-icon-caret-bottom edit_icon" v-else slot="icon"></i>
+						<span>添加食品分类</span>
+					</div>
+	  			</el-form>
+	  			<header class="form_header">添加食品</header>
+	  			<el-form :model="foodForm" :rules="foodRules" ref="foodForm" label-width="110px" class="form food_form">
+	  				<el-form-item label="食品名称" prop="name">
+						<el-input v-model="foodForm.name"></el-input>
+					</el-form-item>
+                    <el-form-item label="食品价格" prop="price">
+                        <el-input placeholder="食品价格参考：5.00" v-model="foodForm.price">
+                            <el-select slot="append" v-model="foodForm.unitId" :placeholder="categoryForm.unitSelect.label"  style="width: 100px;">
+                                <el-option
+                                    v-for="item in categoryForm.unitList"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                                </el-option>
+                            </el-select>
+                        </el-input>
+                    </el-form-item>
+					<el-form-item label="食品详情" prop="details">
+						<el-input v-model="foodForm.details"></el-input>
+					</el-form-item>
+					<el-form-item label="上传食品图片">
+						<el-upload
+						  class="avatar-uploader"
+						  :action="baseUrl + '/api/upload/img'"
+						  :show-file-list="false"
+						  :on-success="uploadImg"
+						  :before-upload="beforeImgUpload" name="imgFile">
+						  <img v-if="foodForm.imgLink" :src="baseUrl + foodForm.imgLink" class="avatar">
+						  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+						</el-upload>
+					</el-form-item>
+					<el-form-item>
+						<el-button type="primary" @click="addFood('foodForm')">确认添加食品</el-button>
+					</el-form-item>
+	  			</el-form>
+  			</el-col>
+  		</el-row>
+    </div>
+</template>
+
+<script>
+ 	import headTop from '@/components/headTop'
+    import {getSpecies, addSpecies, addFood} from '@/api/getData'
+    import {baseUrl} from '@/config/env'
+    export default {
+    	data(){
+    		return {
+    			baseUrl,
+    			categoryForm: {
+    				categoryList: [],
+                    unitList:[{
+                        value: 1,
+                        label: '元/斤'
+                    }, {
+                        value: 2,
+                        label: '元/瓶'
+                    },{
+                        value: 3,
+                        label: '元/袋'
+                    },{
+                        value: 4,
+                        label: '元/只'
+                    },{
+                        value: 5,
+                        label: '元/个'
+                    }],
+                    unitValue:'',
+                    categorySelect: 1,
+                    unitSelect: {
+                        value: 1,
+                        label: '元/斤'
+                    },
+    				name: '',
+    				remarks: '',
+    			},
+                categoryRules:{
+                    speciesId:[
+                        { required: true, message: '请选择食品分类', trigger: 'blur' },
+                    ]
+                },
+    			foodForm: {
+    				name: '',
+    				details: '',
+    				imgLink: '',
+    				speciesId: '',
+    				feature: '',
+    				price: null,
+                    unitId: 1
+    			},
+    			foodRules: {
+
+    				name: [
+						{ required: true, message: '请输入食品名称', trigger: 'blur' },
+					],
+                    price:[
+                        { required: true, message: '请输入食品价格', trigger: 'blur' },
+                    ]
+    			},
+    			showAddCategory: false,
+    			dialogFormVisible: false
+    		}
+    	},
+    	components: {
+    		headTop,
+    	},
+    	created(){
+    		this.initData();
+    	},
+    	computed: {
+    		selectValue: function (){
+    			return this.categoryForm.categoryList[this.categoryForm.categorySelect]||{}
+    		}
+    	},
+    	methods: {
+    		async initData(){
+    			try{
+    				const result = await getSpecies();
+                    console.log(result.data);
+	    			if (result.code == 200) {
+	    				result.data.map((item, index) => {
+	    				    item.index = index;
+	    					item.value = item.id;
+	    					item.label = item.name;
+	    				})
+	    				this.categoryForm.categoryList = result.data;
+                        console.log(this.categoryForm.categoryList);
+	    			}else{
+	    				console.log(result)
+	    			}
+    			}catch(err){
+    				console.log(err)
+    			}
+    		},
+		    addCategoryFun(){
+		    	this.showAddCategory = !this.showAddCategory;
+		    },
+		    submitCategoryForm(categoryForm) {
+				this.$refs[categoryForm].validate(async (valid) => {
+					if (valid) {
+						const params = {
+							name: this.categoryForm.name,
+                            remarks: this.categoryForm.remarks
+						}
+						try{
+							const result = await addSpecies(params);
+							if (result.code == 200) {
+								this.initData();
+								this.categoryForm.name = '';
+								this.categoryForm.remarks = '';
+								this.showAddCategory = false;
+								this.$message({
+					            	type: 'success',
+					            	message: '添加成功'
+					          	});
+							}else {
+                                this.$message({
+                                    type: 'error',
+                                    message: result.message
+                                });
+                            }
+						}catch(err){
+							console.log(err)
+						}
+					} else {
+						this.$notify.error({
+							title: '错误',
+							message: '请检查输入是否正确',
+							offset: 100
+						});
+						return false;
+					}
+				});
+			},
+			uploadImg(res, file) {
+				if (res.code == 200) {
+					this.foodForm.imgLink = res.data;
+				}else{
+					this.$message.error('上传图片失败！');
+				}
+			},
+			beforeImgUpload(file) {
+				const isRightType = (file.type === 'image/jpeg') || (file.type === 'image/png');
+				const isLt2M = file.size / 1024 / 1024 < 2;
+
+				if (!isRightType) {
+					this.$message.error('上传头像图片只能是 JPG 格式!');
+				}
+				if (!isLt2M) {
+					this.$message.error('上传头像图片大小不能超过 2MB!');
+				}
+				return isRightType && isLt2M;
+			},
+			tableRowClassName(row, index) {
+		        if (index === 1) {
+		        	return 'info-row';
+		        } else if (index === 3) {
+		        	return 'positive-row';
+		        }
+		        return '';
+		    },
+		    addFood(foodForm){
+		    	this.$refs[foodForm].validate(async (valid) => {
+					if (valid) {
+						const params = {
+							...this.foodForm,
+							speciesId: this.categoryForm.categorySelect
+						}
+						try{
+							const result = await addFood(params);
+							if (result.code == 200) {
+								console.log(result)
+								this.$message({
+					            	type: 'success',
+					            	message: '添加成功'
+					          	});
+					          	this.foodForm = {
+				    				name: '',
+				    				details: '',
+				    				imgLink: '',
+                                    speciesId: '',
+                                    feature: '',
+				    				price: 0.00,
+                                    unitId: 1
+				    			}
+                                this.$router.push('foodList');
+							}else{
+								this.$message({
+					            	type: 'error',
+					            	message: result.message
+					          	});
+							}
+						}catch(err){
+							console.log(err)
+						}
+					} else {
+						this.$notify.error({
+							title: '错误',
+							message: '请检查输入是否正确',
+							offset: 100
+						});
+						return false;
+					}
+				});
+		    }
+		}
+    }
+</script>
+
+<style lang="less">
+	@import '../../style/mixin';
+	.form{
+		min-width: 400px;
+        width: 100%;
+		margin-bottom: 30px;
+		&:hover{
+			box-shadow: 0 0 8px 0 rgba(232,237,250,.6), 0 2px 4px 0 rgba(232,237,250,.5);
+			border-radius: 6px;
+			transition: all 400ms;
+		}
+	}
+	.food_form{
+		border: 1px solid #eaeefb;
+		padding: 10px 0px;
+	}
+	.form_header{
+		text-align: center;
+		margin-bottom: 10px;
+	}
+	.species_select{
+        width: 100%;
+		border: 1px solid #eaeefb;
+		padding: 10px 30px 10px 10px;
+		border-top-right-radius: 6px;
+		border-top-left-radius: 6px;
+	}
+	.add_category_row{
+		height: 0;
+		overflow: hidden;
+		transition: all 400ms;
+		background: #f9fafc;
+	}
+	.showEdit{
+		height: 185px;
+	}
+	.add_category{
+		background: #f9fafc;
+		padding: 10px 30px 0px 10px;
+		border: 1px solid #eaeefb;
+		border-top: none;
+	}
+	.add_category_button{
+		text-align: center;
+		line-height: 40px;
+		border-bottom-right-radius: 6px;
+		border-bottom-left-radius: 6px;
+		border: 1px solid #eaeefb;
+		border-top: none;
+		transition: all 400ms;
+		&:hover{
+			background: #f9fafc;
+			span, .edit_icon{
+				color: #20a0ff;
+			}
+		}
+		span{
+			.sc(14px, #999);
+			transition: all 400ms;
+		}
+		.edit_icon{
+			color: #ccc;
+			transition: all 400ms;
+		}
+	}
+	.avatar-uploader .el-upload {
+	    border: 1px dashed #d9d9d9;
+	    border-radius: 6px;
+	    cursor: pointer;
+	    position: relative;
+	    overflow: hidden;
+	}
+	.avatar-uploader .el-upload:hover {
+	    border-color: #20a0ff;
+	}
+	.avatar-uploader-icon {
+	    font-size: 28px;
+	    color: #8c939d;
+	    width: 120px;
+	    height: 120px;
+	    line-height: 120px;
+	    text-align: center;
+	}
+	.avatar {
+	    width: 120px;
+	    height: 120px;
+	    display: block;
+	}
+	.cell{
+		text-align: center;
+	}
+</style>
---
+++ pls-manage/src/page/order/subOrderList.vue
@@ -0,0 +1,318 @@
+<template>
+    <div class="fillcontain">
+        <head-top></head-top>
+        <div class="table_container">
+            <div class="demo-input-size">
+                <el-row class="category_select3">
+                    <el-input v-model="queryForm.name" placeholder="主单号/名称" style="width:200px;"></el-input>
+                    <el-date-picker
+                        v-model="queryForm.startTime"
+                        type="datetime"
+                        format="yyyy-MM-dd HH:mm:ss"
+                        placeholder="选择开始日期">
+                    </el-date-picker>
+                    <el-date-picker
+                        v-model="queryForm.endTime"
+                        type="datetime"
+                        format="yyyy-MM-dd HH:mm:ss"
+                        placeholder="选择结束日期">
+                    </el-date-picker>
+                    <el-button @click="handleQuery" type="primary">查询</el-button>
+                </el-row>
+            </div>
+            <div style="margin-bottom: 10px">
+                <el-button @click="handleAdd" type="primary">新增</el-button>
+            </div>
+            <el-table
+                :data="tableData"
+                style="width: 100%"
+                @selection-change="handleSelectionChange">
+                <el-table-column
+                    type="selection"
+                    width="55">
+                </el-table-column>
+                <el-table-column
+                    property="id"
+                    label="主订单号"
+                    width="185">
+                </el-table-column>
+                <el-table-column
+                    property="orderDesc"
+                    label="主订单名称">
+                </el-table-column>
+                <el-table-column
+                    property="totalAmount"
+                    label="总金额">
+                </el-table-column>
+                <el-table-column
+                    property="discountAmount"
+                    label="总优惠金额">
+                </el-table-column>
+                <el-table-column
+                    property="createTime"
+                    label="创建时间"
+                    :formatter="dateFormat">
+                </el-table-column>
+                <el-table-column
+                    property="payStatus"
+                    label="付款状态"
+                    :formatter="formatterPayStatus">
+                </el-table-column>
+                <el-table-column label="操作" width="250">
+                    <template scope="scope">
+                        <el-button
+                            size="small"
+                            @click="handleEdit(scope.row,'edit')">编辑
+                        </el-button>
+                        <el-button
+                            size="small"
+                            @click="handleEdit(scope.row,'edit')">分单管理
+                        </el-button>
+                        <el-button
+                            size="small"
+                            type="danger"
+                            @click="handleDelete(scope.$index, scope.row)">删除
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div class="Pagination" style="text-align: left;margin-top: 10px;">
+                <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage"
+                    :page-size="10"
+                    layout="total, prev, pager, next"
+                    :total="count">
+                </el-pagination>
+            </div>
+            <el-dialog :title="modalTitle" v-model="dialogFormVisible">
+                <el-form :model="selectTable">
+                    <el-form-item label="主单名称" label-width="100px">
+                        <el-input v-model="selectTable.orderDesc" auto-complete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item label="总金额" label-width="100px">
+                        <el-input v-model="selectTable.totalAmount" readonly></el-input>
+                    </el-form-item>
+                    <el-form-item label="优惠金额" label-width="100px">
+                        <el-input v-model="selectTable.discountAmount" readonly></el-input>
+                    </el-form-item>
+                    <el-form-item label="备注" label-width="100px">
+                        <el-input v-model="selectTable.details"></el-input>
+                    </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer">
+                    <el-button @click="dialogFormVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="updateMainOrder">确 定</el-button>
+                </div>
+            </el-dialog>
+        </div>
+    </div>
+</template>
+
+<script>
+    import headTop from '../../components/headTop'
+    import {moment} from '@/config/moment'
+    import {listMainOrder,addMainOrder,deleteMainOrders} from '@/api/getData'
+    export default {
+        data(){
+            return {
+                tableData: [],
+                currentRow: null,
+                limit: 10,
+                count: 0,
+                currentPage: 1,
+                dialogFormVisible: false,
+                name:null,
+                queryForm:{
+                    name:'',
+                    startTime:'',
+                    endTime:''
+                },
+                modalTitle:'',
+                selectTable:{
+                    totalAmount: 0.00,
+                    discountAmount: 0.00
+                },
+                mainOrderIds:[],
+                multipleSelection:[],
+            }
+        },
+        components: {
+            headTop,
+        },
+        created(){
+            this.initData();
+        },
+        methods: {
+            formatterPayStatus(row, column){
+                var statusDesc = "";
+                switch (row.payStatus){
+                    case 1:
+                        statusDesc = "未付款";
+                        break;
+                    case 2:
+                        statusDesc = "付款中";
+                        break;
+                    case 3:
+                        statusDesc = "已付款";
+                    break;
+                }
+                return statusDesc;
+            },
+            //时间格式化
+            dateFormat:function(row, column) {
+                var date = row[column.property];
+                if (date == undefined) {
+                    return "";
+                }
+                return moment(date).format("YYYY-MM-DD HH:mm:ss");
+            },
+            async initData(){
+                try {
+                    this.getMainOrder();
+                } catch (err) {
+                    console.log('获取数据失败', err);
+                }
+            },
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                this.currentPage = val;
+                this.getMainOrder()
+            },
+            handleEdit(row,type){
+                this.getSelectItemData(row);
+                this.dialogFormVisible = true;
+                if(type == 'edit'){
+                    this.modalTitle = '编辑主单信息';
+                }else {
+                    this.modalTitle = '新增主单信息';
+                }
+            },
+            handleQuery(){
+                this.initData();
+            },
+            handleSelectionChange(val) {
+                this.multipleSelection = val;
+            },
+            handleAdd(){
+                this.selectTable = {
+                    id: null,
+                    totalAmount: 0.00,
+                    discountAmount: 0.00,
+                    details: null
+                }
+                this.modalTitle = '新增主单信息';
+                this.dialogFormVisible = true;
+            },
+            async getSelectItemData(row){
+                this.selectTable = row;
+                this.tableData.splice(row.index, 1, {...this.selectTable});
+            },
+            async updateMainOrder(){
+                try {
+                    const MainOrderData = this.selectTable;
+                    const res = await addMainOrder(MainOrderData)
+                    if (res.code == 200) {
+                        this.dialogFormVisible = false;
+                        this.$message({
+                            type: 'success',
+                            message: '更新主单信息成功'
+                        });
+                        this.selectTable = {
+                            id: null,
+                            name: '',
+                            details: '',
+                            phone: '',
+                            address: '',
+                        }
+                        this.getMainOrder();
+                    } else {
+                        this.$message({
+                            type: 'error',
+                            message: res.message
+                        });
+                    }
+                } catch (err) {
+                    console.log('更新主单信息失败', err);
+                }
+            },
+            async handleDelete(index, row) {
+                try {
+                    this.mainOrderIds.push(row.id);
+                    const res = await deleteMainOrders({
+                        mainOrderIds: this.mainOrderIds
+                    });
+                    if (res.code == 200) {
+                        this.$message({
+                            type: 'success',
+                            message: '删除主单成功'
+                        });
+                        this.tableData.splice(index, 1);
+                    } else {
+                        throw new Error(res.message)
+                    }
+                    this.mainOrderIds = [];
+                } catch (err) {
+                    this.$message({
+                        type: 'error',
+                        message: err.message
+                    });
+                    console.log('删除主单失败')
+                }
+            },
+            async getMainOrder(){
+                try {
+                    const res = await listMainOrder({
+                            pageNo: this.currentPage,
+                            pageSize: this.limit,
+                            name:this.queryForm.name,
+                            startTime: this.queryForm.startTime,
+                            endTime: this.queryForm.endTime
+                        });
+                    if (res.code == 200) {
+                        this.count = res.data.total;
+                        this.tableData = [];
+                        res.data.list.forEach((item,index) => {
+                            const tableItem = {
+                                id: item.id,
+                                orderDesc: item.orderDesc,
+                                totalAmount: item.totalAmount,
+                                discountAmount: item.discountAmount,
+                                createTime: item.createTime,
+                                details: item.details,
+                                payStatus: item.payStatus,
+                                index: index
+                            }
+                            this.tableData.push(tableItem);
+                        })
+                        console.log(this.tableData);
+                    } else {
+                        throw new Error(res.message)
+                    }
+                } catch (err) {
+                    console.log('获取主单数据失败', err);
+                }
+            }
+        },
+    }
+</script>
+
+<style lang="less">
+    @import '../../style/mixin';
+
+    .table_container {
+        padding: 20px;
+    }
+    .category_select3{
+        width: 750px;
+        border: 1px solid #eaeefb;
+        padding: 10px 30px 10px 10px;
+        border-top-right-radius: 6px;
+        border-top-left-radius: 6px;
+    }
+</style>
+
+
